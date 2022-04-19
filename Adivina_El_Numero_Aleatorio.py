bd3d8680c38bde11341861eaefc5550b4af4983b
AmetJim123 <88565443+AmetJim123@users.noreply.github.com>
2022-04-19 12:02:48 -0500

    Update Adivina_El_Numero_Aleatorio.py
    now the game can be stop it while you're playing it, and can be restarted once the game have finished or stopped

--- Adivina_El_Numero_Aleatorio.py
+++ Adivina_El_Numero_Aleatorio.py
@@ -11,34 +11,43 @@ from random import *
 
 
 nombre = input("Hola qué tal?, Por favor: Indique su nombre →: ")
-print(f"""Bienvenid@ {nombre} al juego de adivinar un número. Dispondrá de 8 intentos para
- adivinar un número entre 1 y 100. Buena suerte y que acierte al primer intento""")
+print(f"""\nBienvenid@ {nombre} al juego de adivinar un número. Dispondrá de 8 intentos para
+ adivinar un número entre 1 y 100. Buena suerte y que acierte al primer intento\n 
+ [SI EN ALGÚN MOMENTO USTED QUIERE DETENER EL JUEGO, escriba '666']\n""")
 
 
-intentos = 0
-numero_secreto = randint(1, 100)
+jugar = 's'
+while jugar == 's':
 
-while intentos < 8:
+    intentos = 1
+    numero_secreto = randint(1, 100)
+    while intentos <= 8:
 
-    numero = int(input(f"""Bueno, este es su intento número {intentos}. 
+        numero = int(input(f"""Bueno, este es su intento número {intentos}. 
 Digite un número para intentar adivinar el número secreto →: """))
+        if numero == 666:
+            print("\n Veo que ya no quiere jugar, está bien. Adios, cobarde")
+            break
+        elif numero < 1 or numero > 100:
+            intentos += 1
+            print("\nEl número que usted eligió no es válido, ha perdido un intento por ignorante\n")
 
-    if numero < numero_secreto:
-        intentos += 1
-        print(f"El número que usted digitó es menor que el número secreto, vuelva a intentar\n")
+        elif numero < numero_secreto:
+            intentos += 1
+            print("\nEl número que usted digitó es menor que el número secreto, vuelva a intentar\n")
 
-    elif numero > numero_secreto:
-        intentos += 1
-        print("El número que usted digitó es mayor que el número secreto, vuelva a intentar\n")
+        elif numero > numero_secreto:
+            intentos += 1
+            print("\nEl número que usted digitó es mayor que el número secreto, vuelva a intentar\n")
 
-    elif numero not in range(1, 100):
-        intentos += 1
-        print("El número que usted eligió no es válido, ha perdido un intento por ignorante\n")
+        else:
+
+            print(f"""\nFELICIDADES {nombre}! Su número {numero} es igual a el número secreto {numero_secreto}.
+Ganó el juego!\n""")
+            break
 
     else:
-        print(f"""FELICIDADES {nombre}! Su número {numero} es igual a el número secreto {numero_secreto}.
-Ganó el juego!\n""")
-         break
-
-else:
-    print(f"Usted agotó todos sus intentos. GAME OVER. El número secreto era {numero_secreto} ")
+        print(f"Usted agotó todos sus intentos. GAME OVER. El número secreto era {numero_secreto} ")
+    jugar = input("\nEl juego se terminó, ¿Desea volver a jugar?, (S) para sí, (N) para no →: ").lower()
+    if jugar == 'n':
+        print("\nMuchas gracias, vuelva pronto a intentar adivinar el número secreto :D ")
